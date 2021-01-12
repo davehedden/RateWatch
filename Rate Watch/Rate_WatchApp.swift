fc856eeb71ea021b1e0fcefb2668cddfcfaf6094
David Hedden <wavmaker11@gmail.com>
2021-01-12 18:43:14 -0500

    Initial Commit

---
+++ Rate Watch/Rate_WatchApp.swift
@@ -0,0 +1,20 @@
+//
+//  Rate_WatchApp.swift
+//  Rate Watch
+//
+//  Created by Dave Hedden on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Rate_WatchApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
